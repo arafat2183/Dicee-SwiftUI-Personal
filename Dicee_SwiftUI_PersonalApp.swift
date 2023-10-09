89292c685b7adb764534be25f62702a4c9633f14
arafat2183 <akash076987@gmail.com>
2023-10-09 21:10:08 +0900

    Initial commit

---
+++ Dicee_SwiftUI_PersonalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUI_PersonalApp.swift
+//  Dicee-SwiftUI-Personal
+//
+//  Created by Akash Arafat on 2023/10/09.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUI_PersonalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
